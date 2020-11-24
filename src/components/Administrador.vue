ebe562882a3fde8c9ba9c1694ff8953a508bb98e
Natalia <nflores.teoria@gmail.com>
2020-11-24 19:07:58 -0300

    Iniciando proyecto, agregando componente Admin

---
+++ src/components/Administrador.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1 v-text="titulo"></h1>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ["administrador"],
+    computed: {
+        titulo() {
+            return ``;
+        }
+    }  
+}
+</script>
+
+<style scoped>
+    
+</style>
